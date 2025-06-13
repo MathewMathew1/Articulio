f24a4339810911d23a5e222873add771f179577f
Mathew <mateusz.lu123@gmail.com>
2025-06-13 12:08:36 +0200

    updated prod url

--- src/constant/mainRoute.ts
+++ src/constant/mainRoute.ts
@@ -1,3 +1,3 @@
 import { isDevMode } from '@angular/core';
 
-export const MAIN_ROUTE_API =  isDevMode()?  "http://localhost:5012/": "http://articulios.us-east-1.elasticbeanstalk.com/"
+export const MAIN_ROUTE_API =  isDevMode()?  "http://localhost:5012/": "http://articulio.us-east-1.elasticbeanstalk.com/"
